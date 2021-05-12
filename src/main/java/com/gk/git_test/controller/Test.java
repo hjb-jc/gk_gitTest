d3239d1d5727660f8ed01bbd8041c7b87bb68f2a
huangjinba <gk1206@qq.com>
2021-05-13 01:54:41 +0800

    第一次上传

---
+++ src/main/java/com/gk/git_test/controller/Test.java
@@ -0,0 +1,21 @@
+package com.gk.git_test.controller;
+
+public class Test {
+
+    public int age;
+
+    public Test() {
+    }
+
+    public Test(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
